a63da05720f577e66f9a3531c038437a3fa390ea
Mark Klein <kleincentral@gmail.com>
2023-12-07 14:48:16 -0600

    Added favorite.router functionality

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,27 +1,76 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 
 const router = express.Router();
 
 // return all favorite images
-router.get('/', (req, res) => {
-  res.sendStatus(200);
+router.get("/", (req, res) => {
+  const sqlText = `SELECT * FROM "favorites"`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("Error in favorite.router GET,", err);
+      res.sendStatus(500);
+    });
 });
 
 // add a new favorite
-router.post('/', (req, res) => {
-  res.sendStatus(201);
+router.post("/", (req, res) => {
+  const newFavoritedPost = req.body;
+  const sqlText = `
+  INSERT INTO "favorites"
+  ("url")
+  VALUES
+  ($1)`;
+  const sqlValues = newFavoritedPost.newFavorite;
+  pool
+    .query(sqlText, sqlValues)
+    .then((res) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("Error in favorite.router POST,", err);
+      res.sendStatus(500);
+    });
 });
 
 // update a favorite's associated category
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  const sqlText = `
+  UPDATE "favorites"
+    "category_id" = $1
+    WHERE "id" = '${req.params.id}';`;
+
+  const sqlValues = req.body.category_id;
+  pool
+    .query(sqlText, sqlValues)
+    .then((res) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("Error in favorite.router PUT,", err);
+      res.sendStatus(500);
+    });
 });
 
 // delete a favorite
-router.delete('/:id', (req, res) => {
-  res.sendStatus(200);
+router.delete("/:id", (req, res) => {
+  const sqlText = `
+    DELETE FROM "favorites"
+      WHERE "id" = ${req.params.id};`;
+  pool
+    .query(sqlText)
+    .then((res) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("Error in favorite.router DELETE,", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
