516a26a853bfcd0225980ef182022c9e126681c2
Mark Klein <kleincentral@gmail.com>
2023-12-07 13:29:13 -0600

    Added giphyRouter and functionality

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const axios = require("axios");
-const API_KEY = require("../../.env");
+require("dotenv").config();
 
+const API_KEY = process.env.GIPHY_API_KEY;
 const router = express.Router();
 
 router.get("/:id", (req, res) => {
-  console.log("Test GET!", req.params.id);
-  res.sendStatus(200);
+  axios({
+    method: "GET",
+    url: `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&&q=${req.params.id}`,
+  })
+    .then((response) => {
+      res.send(response.data.data);
+    })
+    .catch((error) => {
+      console.log("Error in GET from giphyRouter,", error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
